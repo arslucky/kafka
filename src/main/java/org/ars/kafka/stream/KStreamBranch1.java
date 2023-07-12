2c8751eb44b29228f67ffb422e94b336abef2e58
arslucky <arsibragimov@gmail.com>
2023-07-12 16:45:50 +0300

    added KStreamBranch1

---
+++ src/main/java/org/ars/kafka/stream/KStreamBranch1.java
@@ -0,0 +1,144 @@
+package org.ars.kafka.stream;
+
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Branched;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+/**
+ * @author arsen.ibragimov
+ *
+ *         Split KStream to branches, output to stdout all records and branched
+ */
+public class KStreamBranch1 {
+
+    static Logger log = LogManager.getLogger( KStreamBranch1.class);
+
+    static String topic = KStreamBranch1.class.getSimpleName();
+
+    static final String BOOTSTRAP_SERVERS = "kafka1:9091";
+
+    static {
+        Configurator.setRootLevel( Level.WARN);
+        Configurator.setLevel( "org.apache.kafka.clients.consumer", Level.WARN);
+    }
+
+    static class Producer implements Runnable {
+
+        Properties config = new Properties();
+        KafkaProducer<Integer, Integer> producer = null;
+
+        public Producer() {
+            config.put( ProducerConfig.CLIENT_ID_CONFIG, "producer1");
+            config.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+            config.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
+            config.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
+
+            producer = new KafkaProducer<>( config);
+        }
+
+        @Override
+        public void run() {
+            log.info( "producer:start");
+            try {
+                for( int i = 0; i < 3; i++) {
+                    int key = i;
+                    ProducerRecord<Integer, Integer> record = new ProducerRecord<>( topic, key, i);
+                    producer.send( record);
+                    log.info( String.format( "%s send key:%d, value:%d", currentThread().getName(), key, i));
+                }
+            } catch( Exception e) {
+                e.printStackTrace();
+            } finally {
+                producer.close();
+                log.info( "producer:stop");
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+
+        Properties config = new Properties();
+        KafkaStreams streams = null;
+
+        public Consumer() {
+            config.put( StreamsConfig.APPLICATION_ID_CONFIG, topic + "_client");
+            config.put( StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+            config.put( StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+            config.put( StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+            // rebalance optimization
+            config.put( "internal.leave.group.on.close", "true");
+            config.put( ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "6000");
+        }
+
+        @Override
+        public void run() {
+            try {
+                log.info( "consumer:start");
+
+                StreamsBuilder builder = new StreamsBuilder();
+
+                KStream<Integer, Integer> stream = builder.stream( topic);
+
+                stream.foreach( ( key, value) -> {
+                    log.info( String.format( "%s get key:%d, value:%d", currentThread().getName(), key, value));
+                });
+
+                //@formatter:off
+                Map<String, KStream<Integer, Integer>> branched = stream.split( Named.as( "Branch-"))
+                    .branch( ( key, value) -> key.equals( 0), Branched.as( "zero"))
+                    .branch( ( key, value) -> key.equals( 1), Branched.as( "one"))
+                    .defaultBranch( Branched.as( "default"));
+                //@formatter:on
+
+                for( String branch : branched.keySet()) {
+                    branched.get( branch).foreach( ( key, value) -> {
+                        log.info( String.format( "%s %s key:%d, value:%d", currentThread().getName(), branch, key, value));
+                    });
+                }
+
+                streams = new KafkaStreams( builder.build(), config);
+
+                Runtime.getRuntime().addShutdownHook( new Thread( streams::close));
+
+                streams.start();
+                sleep( 1000);
+            } catch( Exception e) {
+                e.printStackTrace();
+            } finally {
+                streams.close();
+                log.info( "consumer:stop");
+            }
+        }
+    }
+
+    public static void main( String[] args) {
+        try {
+            Thread thread1 = new Thread( new Producer());
+            Thread thread2 = new Thread( new Consumer());
+
+            thread1.start();
+            sleep( 500);
+            thread2.start();
+        } catch( Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
